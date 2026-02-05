2204f58cf1e3d7281260d4e12438670674e37006
Casper Korsgaard <cko@charlietango.dk>
2026-02-05 14:09:26 +0100

    Add constants and improve API configuration handling
    Introduce new constants for enhanced maintainability and readability. Refactor API service registration and Swagger documentation setup, including updated operation filter and improved namespace handling for cleaner implementation.

--- src/CharlieTango.UmbracoTranslations/Composer.cs
+++ src/CharlieTango.UmbracoTranslations/Composer.cs
@@ -1,4 +1,5 @@
 using Asp.Versioning;
+using CharlieTango.UmbracoTranslations.ApiClient.Configuration;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,6 +18,8 @@ public class Composer : IComposer
     public void Compose(IUmbracoBuilder builder)
     {
 
+        builder.Services.AddUmbracoTranslationsApiClient(builder.Config);
+
         builder.Services.AddSingleton<IOperationIdHandler, CustomOperationHandler>();
 
         builder.Services.Configure<SwaggerGenOptions>(opt =>
@@ -27,28 +30,25 @@ public class Composer : IComposer
             // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/versioning-your-api
             // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/access-policies
 
-            // Configure the Swagger generation options
-            // Add in a new Swagger API document solely for our own package that can be browsed via Swagger UI
-            // Along with having a generated swagger JSON file that we can use to auto generate a TypeScript client
             opt.SwaggerDoc(Constants.ApiName, new OpenApiInfo
             {
-                Title = "Charlie Tango Umbraco Translations Backoffice API",
-                Version = "1.0",
-                // Contact = new OpenApiContact
-                // {
-                //     Name = "Some Developer",
-                //     Email = "you@company.com",
-                //     Url = new Uri("https://company.com")
-                // }
+                Title = Constants.ApiTitle,
+                Version = Constants.ApiVersion1,
+                Contact = new OpenApiContact
+                {
+                    Name = Constants.CharlieTangoAuthors,
+                    Email = Constants.CharlieTangoEmail,
+                    Url = new Uri(Constants.CharlieTangoUrl)
+                }
             });
 
             // Enable Umbraco authentication for the "Example" Swagger document
             // PR: https://github.com/umbraco/Umbraco-CMS/pull/15699
-            opt.OperationFilter<CharlieTangoUmbracoTranslationsOperationSecurityFilter>();
+            opt.OperationFilter<UmbracoTranslationsOperationSecurityFilter>();
         });
     }
 
-    public class CharlieTangoUmbracoTranslationsOperationSecurityFilter : BackOfficeSecurityRequirementsOperationFilterBase
+    public class UmbracoTranslationsOperationSecurityFilter : BackOfficeSecurityRequirementsOperationFilterBase
     {
         protected override string ApiName => Constants.ApiName;
     }
@@ -56,15 +56,12 @@ public class Composer : IComposer
     // This is used to generate nice operation IDs in our swagger json file
     // So that the gnerated TypeScript client has nice method names and not too verbose
     // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/umbraco-schema-and-operation-ids#operation-ids
-    public class CustomOperationHandler : OperationIdHandler
+    public class CustomOperationHandler(IOptions<ApiVersioningOptions> apiVersioningOptions)
+        : OperationIdHandler(apiVersioningOptions)
     {
-        public CustomOperationHandler(IOptions<ApiVersioningOptions> apiVersioningOptions) : base(apiVersioningOptions)
-        {
-        }
-
         protected override bool CanHandle(ApiDescription apiDescription, ControllerActionDescriptor controllerActionDescriptor)
         {
-            return controllerActionDescriptor.ControllerTypeInfo.Namespace?.StartsWith("CharlieTango.UmbracoTranslations.Controllers", comparisonType: StringComparison.InvariantCultureIgnoreCase) is true;
+            return controllerActionDescriptor.ControllerTypeInfo.Namespace?.StartsWith(Constants.ApiControllerNamespace, comparisonType: StringComparison.InvariantCultureIgnoreCase) is true;
         }
 
         public override string Handle(ApiDescription apiDescription) => $"{apiDescription.ActionDescriptor.RouteValues["action"]}";
--- src/CharlieTango.UmbracoTranslations/Constants.cs
+++ src/CharlieTango.UmbracoTranslations/Constants.cs
@@ -2,8 +2,13 @@ namespace CharlieTango.UmbracoTranslations;
 
 public sealed class Constants
 {
+    public const string ApiTitle = "Charlie Tango Umbraco Translations Backoffice API";
     public const string ApiVersion1 = "v1";
     public const string ApiName = "charlietangoumbracotranslations";
     public const string ApiGroupName = "CharlieTango.UmbracoTranslations";
+    public const string ApiControllerNamespace = "CharlieTango.UmbracoTranslations.Controllers";
     public const string FrontendTranslationsRootKey = "Frontend";
+    public const string CharlieTangoAuthors = "Casper Korsgaard";
+    public const string CharlieTangoUrl = "https://www.charlietango.dk/";
+    public const string CharlieTangoEmail = "cko@charlietango.dk";
 }
